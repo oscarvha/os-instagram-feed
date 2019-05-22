e1a71435c32041d2e0955f7b74b9e28c41803180
Oscar Sanchez <oscar@bitmarketing.com>
2019-05-22 20:45:52 +0200

    First Commit

---
+++ Model/IntsagramScrap.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oscar
+ * Date: 22/05/19
+ * Time: 20:33
+ */
+
+class InstagramScrap
+{
+    private $instagram;
+
+    private $account;
+
+    /**
+     * InstagramScrapping constructor.
+     * @param string $account
+     */
+    public function __construct(string $account)
+    {
+        $this->instagram = new \InstagramScraper\Instagram();
+        $this->account = $account;
+    }
+
+    /**
+     * @param int $limit
+     * @return \InstagramScraper\Model\Media[]
+     * @throws \InstagramScraper\Exception\InstagramException
+     * @throws \InstagramScraper\Exception\InstagramNotFoundException
+     */
+    public function getMedias(int $limit = 16) : array
+    {   $mediasArray = [];
+        $medias = $this->instagram->getMedias($this->account,$limit);
+
+        foreach($medias as $media) {
+            $mediasArray[] = [
+                'id' => $media->getId(),
+                'link' => $media->getLink(),
+                'type' => $media->getType(),
+                'video' => $media->getVideoLowResolutionUrl(),
+                'image' => $media->getImageThumbnailUrl(),
+                'title' => $media->getCaption()
+            ];
+        }
+
+        return $mediasArray;
+    }
+
+}
